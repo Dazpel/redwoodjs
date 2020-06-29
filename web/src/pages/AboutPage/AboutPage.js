3331f81323c9dd0c6661c0e16a75c1509b3cdcfe
Dazpel <alexxdav97@gmail.com>
2020-06-28 21:32:27 -0400

    First Commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return <BlogLayout>About Page</BlogLayout>
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>Home Page</BlogLayout>
+}
+
+export default HomePage
