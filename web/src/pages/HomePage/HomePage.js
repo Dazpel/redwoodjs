9fa1c95656ca84c94203f1b704a85ca12470f248
Dazpel <alexxdav97@gmail.com>
2020-07-01 20:42:02 -0400

    posts,blog added

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,12 @@
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell/BlogPostsCell'
 
 const HomePage = () => {
-  return <BlogLayout>Home Page</BlogLayout>
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
 }
 
 export default HomePage
